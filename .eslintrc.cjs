b9a7409651cc4fb244feb452758c0714a1259c8a
Victor Pogor <victor.pogor@outlook.com>
2024-04-23 12:31:11 +0000

    feat(auth): added MSAL integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,27 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    'ecmaVersion': 'latest',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    'import/resolver': {
+      'typescript': {}
+    }
+  }
 }
---
+++ src/authConfig.ts
@@ -0,0 +1,78 @@
+import { Configuration, LogLevel } from '@azure/msal-browser';
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: 'f1041483-e6ea-4f04-a602-5cad20a33b36', // This is the ONLY mandatory field that you need to supply.
+    authority:
+      'https://login.microsoftonline.com/f39d70f4-8b8a-46f1-ad1d-eb36979e6c4e', // Defaults to "https://login.microsoftonline.com/common"
+    redirectUri: window.location.origin, // Points to window.location.origin. You must register this URI on Azure Portal/App Registration.
+    postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
+    navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+  },
+  cache: {
+    cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
+    },
+  },
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+  scopes: ['openid', 'profile', 'email'],
+};
+
+/**
+ * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const protectedResources = {
+  apiTodoList: {
+    endpoint: 'http://localhost:5000/api/todolist',
+    scopes: {
+      read: ['api://Enter_the_Web_Api_Application_Id_Here/Todolist.Read'],
+      write: ['api://Enter_the_Web_Api_Application_Id_Here/Todolist.ReadWrite'],
+    },
+  },
+};
+
+/**
+ * An optional silentRequest object can be used to achieve silent SSO
+ * between applications by providing a "login_hint" property.
+ */
+export const silentRequest = {
+  scopes: ['openid', 'profile', 'email'],
+};
